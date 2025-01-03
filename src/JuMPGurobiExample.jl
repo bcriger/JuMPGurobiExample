96321c927b284522411a299d94f4a983ecba68c6
Ben Criger <bcriger@gmail.com>
2025-01-03 13:46:30 +0000

    first commit, just putting up the source that's supposed to work

--- src/JuMPGurobiExample.jl
+++ src/JuMPGurobiExample.jl
@@ -1,5 +1,43 @@
 module JuMPGurobiExample
 
-# Write your package code here.
+import JuMP
+import MathOptInterface as MOI
+
+#=
+    I have a big computer with a Gurobi license, and a small one with
+    no license.
+    I want to do my dev work on small computer, and big runs on the big
+    one, so I want to take advantage of the generic MathOptInterface to
+    switch optimization libraries, using Gurobi where available, and
+    defaulting to HiGHS if not.
+=#
+
+try
+    # see ->-v
+    # jump.dev/JuMP.jl/stable/packages/Gurobi/#Reusing-the-same-Gurobi-environment-for-multiple-solves
+    
+    import Gurobi
+    
+    global const gurobi_env = Ref{Gurobi.Env}()
+    # global const gurobi_env = Gurobi.Env()
+    
+    # function __init__()
+    #     global gurobi_env
+    #     gurobi_env[] = Gurobi.Env()
+    #     return
+    # end
+
+    # assure an error will be generated without a valid license
+    # _ = Gurobi.Env()
+
+    # _ = JuMP.Model(() -> Gurobi.Optimizer(gurobi_env[]))
+
+    global const OPTIMIZATION_MODULE = "Gurobi"
+catch
+    import HiGHS
+    global const OPTIMIZATION_MODULE = "HiGHS"
+end
 
 end
+
+include("solve_two_ILPs.jl")
---
+++ src/solve_two_ILPs.jl
@@ -0,0 +1,49 @@
+#=
+	I want to be able to use one Gurobi license to solve two problems
+	back-to-back.
+=#
+
+function new_ilp()
+	ilp = nothing
+
+	if OPTIMIZATION_MODULE == "HiGHS"
+		ilp = JuMP.Model(HiGHS.Optimizer)
+		HiGHS.Highs_resetGlobalScheduler(1)
+	elseif OPTIMIZATION_MODULE == "Gurobi"    
+		ilp = JuMP.Model(() -> Gurobi.Optimizer(gurobi_env[]))
+	end
+	
+	#=
+	jump.dev/JuMP.jl/stable/tutorials/getting_started/getting_started_with_JuMP/#An-example
+	=#
+
+	#=
+	Note: I get an error when I call 
+	JuMP.@variable(ilp, x >= 0, JuMP.Int)
+	but not 
+	JuMP.@variable(ilp, x >= 0, Int)
+	
+	For now, I ignore this issue.
+	=#
+
+	JuMP.@variable(ilp, x >= 0, Int)
+	JuMP.@variable(ilp, 0 <= y <= 3, Int)
+	
+	coeff_1 = mod(rand(JuMP.Int), 20)
+	coeff_2 = mod(rand(JuMP.Int), 20)
+
+	JuMP.@objective(ilp, Min, coeff_1 * x + coeff_2 * y)
+
+	JuMP.@constraint(ilp, c1, 6x + 8y >= 100)
+
+	JuMP.@constraint(ilp, c2, 7x + 12y >= 120)
+
+end
+
+function solve_two_ilps()
+	ilp_1 = new_ilp()
+	JuMP.optimize!(ilp_1)
+
+	ilp_2 = new_ilp()
+	JuMP.optimize!(ilp_2)
+end
